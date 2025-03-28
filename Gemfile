f3ee129d8dfb8472db0f86876f2f8d871cd99455
ChoiceG <efechoice@gmail.com>
2025-03-28 21:00:21 +0100

    Added Admin Page and Configured

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem "jbuilder"
 gem "devise"
 # Use to style icons
 gem "font-awesome-sass"
+gem "font-awesome-rails"
+
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  authenticated :admin do # Corrected: use :admin instead of :admin_user
+    root to: "admin#index", as: :admin_root
+  end
+
   root to: "home#index"
+
+  get "admin" => "admin#index"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
