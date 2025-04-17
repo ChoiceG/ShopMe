9fc4ca12dc9314371139fc0d10243e16d7501656
ChoiceG <efechoice@gmail.com>
2025-04-17 19:48:56 +0100

    Set up the shopping cart controller, view, route, and links

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :categories, only: [ :show ]
   resources :products, only: [ :show ]
   get "about" => "pages#about"
+  get "cart" => "carts#show"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
