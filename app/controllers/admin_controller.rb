4a3216d5ae22da7a3924aa070184f14edc2fd003
ChoiceG <efechoice@gmail.com>
2025-04-24 15:23:22 +0100

    Updated admin structure

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -35,6 +35,34 @@
 #     end
 # end
 
+# class AdminController < ApplicationController
+#   layout "admin"
+#   before_action :authenticate_admin!
+
+#   def index
+#     # fetch last five (5) unfulfilled orders
+#     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
+
+#     # add a hash called quick_stats
+#     today_start_of_day_utc = Time.zone.now.utc.beginning_of_day
+#     today_end_of_day_utc = Time.zone.now.utc.end_of_day
+
+#     @quick_stats = {
+#       sales: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).count,
+#       revenue: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).sum(:total).to_f.round,
+#       avg_sale: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).average(:total).to_f.round,
+#       per_sale: OrderProduct.joins(:order).where(orders: { created_at: today_start_of_day_utc..today_end_of_day_utc }).average(:quantity).to_f.round
+#     }
+
+#     # graph- line chart that shows how much revenue we have made each day for the last seven days
+#     @orders_by_day = Order.where("created_at >= ?", 7.days.ago.beginning_of_day.utc).order(:created_at)
+#     @revenue_by_day_grouped = @orders_by_day.group_by { |order| order.created_at.to_date }
+#     @revenue_by_day = (6.days.ago.to_date..Date.today).map do |date|
+#       [ date.strftime("%A"), @revenue_by_day_grouped.fetch(date, []).sum(&:total).to_f.round ]
+#     end
+#   end
+# end
+
 class AdminController < ApplicationController
   layout "admin"
   before_action :authenticate_admin!
@@ -49,8 +77,8 @@ class AdminController < ApplicationController
 
     @quick_stats = {
       sales: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).count,
-      revenue: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).sum(:total).to_f.round,
-      avg_sale: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).average(:total).to_f.round,
+      revenue: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).sum("total / 100.0").to_f.round,  # Converting kobo to naira
+      avg_sale: Order.where(created_at: today_start_of_day_utc..today_end_of_day_utc).average("total / 100.0").to_f.round,  # Converting kobo to naira
       per_sale: OrderProduct.joins(:order).where(orders: { created_at: today_start_of_day_utc..today_end_of_day_utc }).average(:quantity).to_f.round
     }
 
@@ -58,7 +86,7 @@ class AdminController < ApplicationController
     @orders_by_day = Order.where("created_at >= ?", 7.days.ago.beginning_of_day.utc).order(:created_at)
     @revenue_by_day_grouped = @orders_by_day.group_by { |order| order.created_at.to_date }
     @revenue_by_day = (6.days.ago.to_date..Date.today).map do |date|
-      [ date.strftime("%A"), @revenue_by_day_grouped.fetch(date, []).sum(&:total).to_f.round ]
+      [ date.strftime("%A"), @revenue_by_day_grouped.fetch(date, []).sum { |order| order.total / 100.0 }.to_f.round ] # Converting kobo to naira
     end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
   has_many :order_products
   accepts_nested_attributes_for :order_products, reject_if: :all_blank, allow_destroy: true
+
+  def total_in_naira
+    total / 100.0 if total.present?
+  end
 end
