7df867df2b76ff04010f0ab82e673348729d9276
ChoiceG <efechoice@gmail.com>
2025-05-08 23:42:24 +0100

    Updated the navbar mobile

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,23 +3,25 @@ import "controllers";
 // import 'font-awesome/css/all.css';
 // import "cocoon-js";
 
-// import { Application } from "@hotwired/stimulus";
+import { Application } from "@hotwired/stimulus";
 // import DashboardController from "controllers/dashboard_controller"; // Correct import using the importmap name
 // import ProductsController from "controllers/products_controller"; // Correct import using the importmap name
 // import CartController from "controllers/cart_controller";
 // import AlertController from "controllers/alert_controller";
 // import CarouselController from "controllers/carousel_controller";
 // import DataMethodController from "controllers/data_method_controller;"
+import MobileNavController from "controllers/mobile_nav_controller";
 
-// const application = Application.start();
+const application = Application.start();
 // application.register("dashboard", DashboardController);
 // application.register("products", ProductsController);
 // application.register("cart", CartController);
 // application.register("alert", AlertController);
 // application.register("carousel", CarouselController)
 // application.register("data_method", DataMethodController);
-// application.debug = true;
-// window.Stimulus = application;
-// window.application = application;
+application.register("mobile_nav", MobileNavController);
+application.debug = true;
+window.Stimulus = application;
+window.application = application;
 
 
